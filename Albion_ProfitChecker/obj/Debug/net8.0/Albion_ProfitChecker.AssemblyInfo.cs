8b334c2c1d658ebba69eea82bde0e8c0a267b95f
LeonWWImmo <leon.neuhaus@wwimmo.ch>
2026-01-07 09:09:00 +0100

    #0038 Dashboard speed & adding icon

--- Albion_ProfitChecker/obj/Debug/net8.0/Albion_ProfitChecker.AssemblyInfo.cs
+++ Albion_ProfitChecker/obj/Debug/net8.0/Albion_ProfitChecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a9be879c98a3d92763bf960e6ff4c228164fd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9109d8fd41162381e6ab15baa58173851b49d8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
